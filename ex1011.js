0c75bd30d1d720c0455284e3c3430c2663646d00
MichaelOliveira13 <micayl95@gmail.com>
2022-06-07 21:52:08 -0300

    primeiro commit

---
+++ ex1011.js
@@ -0,0 +1,19 @@
+var R = 3
+var pi = 3.14159
+var forml = (4.0/3) * pi * (R**3)
+
+console.log("VOLUME = "+forml.toFixed(3))
+console.log(" ")
+
+var R = 15
+var pi = 3.14159
+var forml = (4.0/3) * pi * (R**3)
+
+console.log("VOLUME = "+forml.toFixed(3))
+console.log(" ")
+
+var R = 1523
+var pi = 3.14159
+var forml = (4.0/3) * pi * (R**3)
+
+console.log("VOLUME = "+forml.toFixed(3))
