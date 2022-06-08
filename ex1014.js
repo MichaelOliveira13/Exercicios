55fea1af8224144814d563c493afc9a01e9fbd8b
MichaelOliveira13 <micayl95@gmail.com>
2022-06-07 21:53:32 -0300

    segundo commit

--- ex1014.js
+++ ex1014.js
@@ -1,23 +1,17 @@
 var X = 500
 var Y = 35.0
-
 var res = X / Y
-
 
 console.log( res . toFixed(3),'km/l');
 
 var X = 2254
 var Y = 124.4
-
 var res = X / Y
-
 
 console.log( res . toFixed(3),'km/l');
 
 var X = 4554
 var Y = 464.6
-
 var res = X / Y
-
 
 console.log( res . toFixed(3),'km/l');
--- ex1035.js
+++ ex1035.js
@@ -3,7 +3,6 @@ var A = 5
 var B = 6
 var C = 7
 var D = 8
-
 //&& -> Sempre usar para "adicionar" mais de um elemento
 if(B>C && D>A && C+D>A+B && C,D > 0 && A % 2 == 0){ 
     console.log("Valores Aceitos")
@@ -16,7 +15,6 @@ var A = 2
 var B = 3
 var C = 2
 var D = 6
-
 if(B>C && D>A && C+D>A+B && C,D > 0 && A % 2 == 0){
     console.log("Valores Aceitos")
 } else{
