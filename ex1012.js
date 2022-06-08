a4f0f3c2c7bfe85cd0e8be0d671b6fa9cf9072ef
MichaelOliveira13 <micayl95@gmail.com>
2022-06-07 22:45:47 -0300

    primeiro commit

---
+++ ex1012.js
@@ -0,0 +1,30 @@
+var A = 3
+var B = 4
+var C = 5.2
+var pi = 3.14159
+var tri = (A * C)/2
+var circ = pi * (C**2)
+var trap = ((A+B)*C)/2
+var quad = B*B
+var retan = A*B
+console.log("TRIANGULO: " + tri.toFixed(3))
+console.log("CIRCULO: " + circ.toFixed(3))
+console.log("TRAPEZIO: " + trap.toFixed(3))
+console.log("QUADRADO: " + quad.toFixed(3))
+console.log("RETANGULO: " + retan.toFixed(3))
+console.log(" ")
+
+var A = 12.7
+var B = 10.4
+var C = 15.2
+var pi = 3.14159
+var tri = (A * C)/2
+var circ = pi * (C**2)
+var trap = ((A+B)*C)/2
+var quad = B*B
+var retan = A*B
+console.log("TRIANGULO: " + tri.toFixed(3))
+console.log("CIRCULO: " + circ.toFixed(3))
+console.log("TRAPEZIO: " + trap.toFixed(3))
+console.log("QUADRADO: " + quad.toFixed(3))
+console.log("RETANGULO: " + retan.toFixed(3))
