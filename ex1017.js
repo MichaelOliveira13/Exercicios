e96a07757e70666453d2b717fd2309cadeee05c4
MichaelOliveira13 <micayl95@gmail.com>
2022-06-09 23:09:40 -0300

    primeiro commit

---
+++ ex1017.js
@@ -0,0 +1,19 @@
+var velo = 12 
+var time = 10
+var vmed = 85
+var res = (vmed / velo) * time
+console.log(res .toFixed(3))
+console.log(" ")
+
+var velo = 12 
+var time = 2
+var vmed = 92
+var res = (vmed / velo) * time
+console.log(res .toFixed(3))
+console.log(" ")
+
+var velo = 12 
+var time = 22
+var vmed = 67
+var res = (vmed / velo) * time
+console.log(res .toFixed(3))
