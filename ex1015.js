9b22473bab195755b5eff7f2192b89579bb3eb9f
MichaelOliveira13 <micayl95@gmail.com>
2022-06-09 22:51:03 -0300

    primeiro commit

---
+++ ex1015.js
@@ -0,0 +1,25 @@
+var x1 = 1
+var x2 = 5
+var y1 = 7
+var y2 = 9
+var form = ((x2 - x1)**2) + ((y2 - y1)**2)
+var raiz = Math.sqrt(form)
+console.log(raiz .toFixed(4))
+console.log(" ")
+
+var x1 = -2.5
+var x2 = 12.1
+var y1 = 0.4
+var y2 = 7.3
+var form = ((x2 - x1)**2) + ((y2 - y1)**2)
+var raiz = Math.sqrt(form)
+console.log(raiz .toFixed(4))
+console.log(" ")
+
+var x1 = 2.5
+var x2 = -12.2
+var y1 = -0.4
+var y2 = 7
+var form = ((x2 - x1)**2) + ((y2 - y1)**2)
+var raiz = Math.sqrt(form)
+console.log(raiz .toFixed(4))
